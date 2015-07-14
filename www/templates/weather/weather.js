09b70bb1b4d4446270e97e5bff09a09ee6960919
Scott Humphries <scott@sscotth.io>
2015-07-14 15:17:04 -0500

    get weather with cors proxy

---
+++ www/templates/weather/weather.js
@@ -0,0 +1,13 @@
+angular.module('won.weather', [])
+
+  .controller('WeatherCtrl', function ($scope, $stateParams, $http) {
+
+    $scope.city = $stateParams.city;
+
+    $http
+      .get('/api/forecast/' + $stateParams.lat + ',' + $stateParams.long)
+      .success(function (data) {
+        console.log(data);
+      });
+
+  });
