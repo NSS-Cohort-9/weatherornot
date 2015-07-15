740fa31f90fd4e657d6075b008f0d5d4e4351cb5
Scott Humphries <scott@sscotth.io>
2015-07-15 11:24:11 -0500

    add weather display

--- www/templates/weather/weather.js
+++ www/templates/weather/weather.js
@@ -1,13 +1,20 @@
 angular.module('won.weather', [])
 
-  .controller('WeatherCtrl', function ($scope, $stateParams, $http) {
+  .controller('WeatherCtrl', function ($scope, $stateParams, $http, $ionicLoading) {
 
     $scope.city = $stateParams.city;
+
+    $ionicLoading.show({
+      template: '<img src="http://i.imgur.com/EeE1Lsp.gif"><h1>Loading...</h1>'
+    });
 
     $http
       .get('/api/forecast/' + $stateParams.lat + ',' + $stateParams.long)
       .success(function (data) {
-        console.log(data);
+        setTimeout(function () {
+          $scope.current = data.currently;
+          $ionicLoading.hide();
+        }, 2000);
       });
 
   });
